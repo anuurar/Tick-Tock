4c72d1ed46726af72964f2b1b190cc951535499f
anuurar <anuragkarki24@gmail.com>
2026-03-03 01:26:01 +0545

    Converted the time which is in seconds to years, days, minutes, and seconds

--- TickTock.c
+++ TickTock.c
@@ -2,14 +2,33 @@
 #include<time.h>
 
 int main()
-{   
+{
+    int s,m,h,d,y;   
     time_t now;
     long t;
 
     now = time(NULL);//Passing NULL to only get a return value.
     t = (long)now;
 
-    printf("Tiem in seconds since epoch: %ld seconds.",t);
+    printf("Time in seconds since epoch: %ld seconds.",t);
+
+    //Converting the time in seconds to years, days, hours, minutes and seconds.
+
+    s = t % 60;//stores excess seconds in s.
+    t = t / 60;//converts t to complete mins only.
+
+    m = t % 60;//stores excess minutes in m.
+    t = t / 60;//converts t to complete hours only.
+
+    h = t % 24;//stores excess hours in h.
+    t = t / 24;//converts t to complete days only.
+
+    d = t % 365;//stores excess days in d.
+    t = t / 365;//converts t to complete years only.
+
+    y = t;//store the no. of years in y.
+    
+    printf("\nTime since epoch is %d years, %d days, %d hours, %d minutes, %d seconds.",y,d,h,m,s);
 
     return 0;
 }
