324aa9f8d08501ebff6b5a8640e4ca9d27b2882e
anuurar <anuragkarki24@gmail.com>
2026-03-03 01:01:21 +0545

    chore: Renamed readme.md to basic documantation.txt and better formatted the output for time since epoch.

--- TickTock.c
+++ TickTock.c
@@ -2,12 +2,14 @@
 #include<time.h>
 
 int main()
-{
+{   
     time_t now;
     long t;
+
     now = time(NULL);//Passing NULL to only get a return value.
     t = (long)now;
-    printf("%ld",t);
+
+    printf("Tiem in seconds since epoch: %ld seconds.",t);
 
     return 0;
 }
