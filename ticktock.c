54362a4485556b68dde1edc83673c9517b7d8782
anuurar <anuragkarki24@gmail.com>
2026-03-08 02:19:20 +0545

    Added a functioning alarm clock system

--- ticktock.c
+++ ticktock.c
@@ -5,21 +5,31 @@
 #include<time.h>
 #include<dos.h>
 
+int count_alarms=0;//Global count of number of alarms set.
+long alarm_seconds[100];//Global array to store alarms in seconds only from 00:00:00.
+
+void initialize();//Function to initiakize clock settings and alarms
 void UTCclock();
 void timer();
 void stopwatch();
 void setAlarm();
+void checkAlarm();
 int checkExit();//Loop control function that checks for esc key press
+void ring();//Rings until a key is pressed.
 
 int main()
 {
 	int key = 0;
-	while(key!=27)
+	clrscr();
+
+	initialize();//Initializing the clock settings and alarm timigs.
+
+	while(key!= 27)
 	{
 		clrscr();
 
-		if(kbhit()) getch();//Buffer filter.
-		//Right now this filter is used to filter the  excess input from timer.
+		//if(kbhit()) getch();//Buffer filter.
+		//This filter was used to filter the  excess input from timer.
 
 		printf("What do you want to do?");
 		printf("\n Press 1 for clock");
@@ -33,12 +43,31 @@ int main()
 		else if(key == 50) timer();
 		else if(key == 51) stopwatch();
 		else if(key == 52) setAlarm();
-		else if(key == 27) printf("\nExiting...");
-		else printf("\nInvalid Input!");
-
-		delay(500);
+		else if(key == 27) {printf("\nExiting...");delay(500);}
+		else {printf("\nInvalid Input!");delay(500);}
 	}
 	return 0;
+}
+
+void initialize()
+{
+	FILE *fp;
+
+	fp = fopen("Alarms.dat","rb");
+
+	if(fp)//If file exists.
+	{
+		fread(&count_alarms,sizeof(count_alarms),1,fp);
+		fread(alarm_seconds,sizeof(alarm_seconds),count_alarms,fp);
+	}
+	else
+	{
+		printf("Aww!! There are no alarms set... Why not set some? :D");
+		delay(2000);
+		clrscr();
+	}
+
+	fclose(fp);
 }
 
 void UTCclock()
@@ -50,6 +79,7 @@ void UTCclock()
 	while(checkExit())
 	{
 		clrscr();
+		checkAlarm();//Checks if any alarm nneds to be rung.
 
 		now = time(NULL);//Pass null to just get the seconds
 
@@ -62,7 +92,7 @@ void UTCclock()
 		m = t % 60; //Storing excess minutes in m.
 		t = t / 60; //Converting t to complete hours only.
 
-		h = h % 24; //Storing excess hours in h.
+		h = t % 24; //Storing excess hours in h.
 
 		printf("%02d:%02d:%02d",h,m,s);
 		delay(50);
@@ -75,6 +105,7 @@ void timer()
 	double time_elapsed = 0.0,wait_time,time_left;
 
 	clrscr();
+
 	printf("Enter no. of secods to wait:");
 	scanf("%lf",&wait_time);
 
@@ -83,49 +114,110 @@ void timer()
 	while(checkExit())
 	{
 		clrscr();
+		checkAlarm();
+
 		now = clock();//Storing current clock ticks.
 
-		time_elapsed = (double)(now - start) / CLOCKS_PER_SEC;//calculating time elspsed in seconds.
+		time_elapsed = (double)(now - start) / CLOCKS_PER_SEC;//Calculating time elspsed in seconds.
 
 		time_left = wait_time - time_elapsed;
 
 		printf("%.3lf",time_left);
 		delay(50);
 
-		if(time_left<=0.0) {printf("yo");break;}
+		if(time_left<=0.0) break;
 	}
 
 	//this filter was put just incase the above loop had any excess input.
 	//if(kbhit()) getch();//Buffer filter.
 
-	while(!kbhit())
-	{
-		sound(800);//plays a beep of 800 Hzs.
-		delay(200);
-		nosound();//Stops the sound.
-		delay(800);
-	}
+	printf("Time's Up!!");
+	ring();
 }
 
 void stopwatch()
 {
-	printf("\nStopwatch");
+	clrscr();
+
+	while(checkExit())
+	{
+		clrscr();
+		checkAlarm();
+
+		printf("\nStopwatch");
+		delay(50);
+	}
 }
 
 void setAlarm()
 {
-	printf("\nAlarm Clock");
+	FILE *fp;
+	int h,m;
+
+	clrscr();
+
+	fp = fopen("Alarms.dat","wb");
+
+	clrscr();
+	checkAlarm();
+
+	printf("\nEnter the Alarm hour and minutes you want to set:");
+	scanf("%d %d", &h, &m);
+
+	alarm_seconds[count_alarms] = h*3600 + m*60;//Converting the alarm time to seconds only.
+	count_alarms++;
+	fwrite(&count_alarms,sizeof(count_alarms),1,fp);
+	fwrite(alarm_seconds,sizeof(alarm_seconds),count_alarms,fp);
+
+	fclose(fp);
+}
+
+void checkAlarm()
+{
+	long t,current_seconds;
+    time_t now;
+	int i;
+
+    now = time(NULL);
+    t =(long)now;
+
+    current_seconds = t % 86400;//Gets the seconds passed today
+
+	for(i=0;i<count_alarms;i++)
+	{
+	if(alarm_seconds[i] == current_seconds)
+	{
+		ring();
+	}
+	}
 }
 
 int checkExit()
 {
 	int key = 0;
 
-	if(kbhit())
-		key = getch();
+	if(kbhit()) key = getch();
 
 	if(key == 27)
 		return 0;
 	else
 		return 1;
+}
+
+void ring()
+{
+	while(!kbhit())
+	{
+		sound(800);//Plays a beep of 800 Hzs.
+		delay(200);
+		nosound();//Stops the sound.
+		delay(800);
+	}
+
+	//This buffer filter replaces all else filter.
+	if(kbhit()) getch();//Buffer filter.
+
+	sound(650);
+	delay(300);
+	nosound();
 }
